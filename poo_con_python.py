40142bca2c042a2067694b130528274d4de4d443
CassBo <karmeterskate@gmail.com>
2025-01-17 08:47:14 -0600

    Problema celestial

--- poo_con_python.py
+++ poo_con_python.py
@@ -1,11 +1,5 @@
-class Personaje:
-#Atributos de la clase
-    #nombre = 'Default'
-    #fuerza = 10
-    #inteligencia = 10
-    #defensa = 10
-    #vida = 10
 
+class Personaje:
     def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
         self.__nombre = nombre
         self.__fuerza = fuerza
@@ -45,17 +39,79 @@ class Personaje:
     def set_fuerza(self,fuerza):
         self.__fuerza = fuerza
 
+    def get_inteligencia(self):
+        return self._inteligencia
 
-
+    def get_defensa(self):
+        return self._defensa
 
 class Guerrero(Personaje):
-    def _init_(self, nombre, fuerza, inteligencia, defensa, vida, espada):
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, espada):
         super().__init__(nombre, fuerza, inteligencia, defensa, vida)
         self.espada = espada
-Hércules = Guerrero("Hércules", 80, 50, 100, 100)
-Hércules.atributos
+
+    def cambiar_arma(self):
+        opcion = int(input("Elige un arma: \n(1) Espada de plata, daño 10. \n(2) Espada de bronce, daño 8."))
+        if opcion == 1:
+            self.espada = 10
+        elif opcion == 2:
+            self.espada = 8
+        else:
+            print("Valor incorrecto")
+
+    # Sobrescribir método
+    def atributos(self):
+        super().atributos()
+        print("-Espada:", self.espada)
+
+    # Sobrescribir daño
+    def dañar(self, enemigo):
+        return self.get_fuerza() * self.espada - enemigo.get_defensa()
 
 
+class Mago(Personaje):
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, libro):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.libro = libro
+
+    def cambiar_arma(self):
+        opcion = int(input("Elige un arma: \n(1) Varita de roble, daño 10. \n(2) Varita de padalustro, daño 8."))
+        if opcion == 1:
+            self.libro = 10
+        elif opcion == 2:
+            self.libro = 8
+        else:
+            print("Valor incorrecto")
+
+    # Sobrescribir método
+    def atributos(self):
+        super().atributos()
+        print("-Libro:", self.libro)
+
+    # Sobrescribir daño
+def dañar(self, enemigo):
+        return self.get_inteligencia() * self.libro - enemigo.get_defensa()
+
+
+# Crear instancias de los personajes
+trakalosa = Personaje("La trakalosa de Monterrey", 20, 15, 10, 100)
+hercules = Guerrero("Hércules", 20, 15, 10, 100, espada=5)  # Proporcionar un valor para espada
+dios = Mago("Diosito", 20, 15, 10, 100, libro=5)  # Proporcionar un valor para libro
+
+# Mostrar atributos
+trakalosa.atributos()
+hercules.atributos()
+dios.atributos()
+
+# Ataques
+trakalosa.atacar(hercules)
+hercules.atacar(dios)
+dios.atacar(trakalosa)
+
+#
+trakalosa.atributos()
+hercules.atributos()
+dios.atributos()
 
 
 
@@ -68,15 +124,15 @@ Hércules.atributos
 
 
 #Variable del constructor de vacío de la clase
-mi_personaje = Personaje("trakalosa de monterrey", 70, 90, 50, 100)
-mi_enemigo = Personaje("La arrolladora banda el limon", 60, 90, 40, 100)
+#mi_personaje = Personaje("trakalosa de monterrey", 70, 90, 50, 100)
+#mi_enemigo = Personaje("La arrolladora banda el limon", 60, 90, 40, 100)
 #print(mi_personaje.dañar(mi_enemigo))
 
 #print(mi_personaje.esta_vivo())
 #mi_personaje.morir()
-mi_personaje.atributos()
-mi_personaje.atacar(mi_enemigo)
-mi_enemigo.atributos()
+#mi_personaje.atributos()
+#mi_personaje.atacar(mi_enemigo)
+#mi_enemigo.atributos()
 #mi_personaje.subir_nivel(10,10,0)
 #mi_personaje.atributos()
 
